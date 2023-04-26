dbeb44e9b9114d544fb73c3fdfa5843942ca4256
Saurav Khairnar <124131200+sauravkhairnar@users.noreply.github.com>
2023-04-26 14:44:31 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,27 @@
+// alert("congratulations!")
+// alert("Hello")
+// alert(4+1);
+// let num=15;
+//     alert(num)
+
+// const num1=14;
+//     alert(num1)
+// num1=13;
+//     alert(num1)
+
+/*let a=2;
+   a*=5;
+    alert(a);
+var res=12==0;
+    alert(res)
+let x=6;
+let y=13;
+        alert(x<10 && y>3)
+if(x>y)
+    alert("shreya")
+else
+    alert("thorat")
+alert(typeof(true));
+let age=prompt("what is ypur age?",100)
+    alert("your age is:"+age)
+*/
